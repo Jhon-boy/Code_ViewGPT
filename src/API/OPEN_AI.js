b27cf18874c69e40ae04daaed26be6503e8687c7
Jhon-boy <jhoncuvi12@gmail.com>
2023-11-27 21:59:37 -0500

    Actualizacion 27/11/2023 2.0

--- src/API/OPEN_AI.js
+++ src/API/OPEN_AI.js
@@ -1,2 +1,2 @@
-export const URL_API = 'http://localhost:8080/codeView';
-// export const URL_API = 'https://codeview-api-0vkr.onrender.com/codeView';
+// export const URL_API = 'http://localhost:8080/codeView';
+export const URL_API = 'https://codeview-api-0vkr.onrender.com/codeView';
