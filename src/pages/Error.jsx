e68b22d3852052e48a93942d7c91221754077791
Jhon-boy <jhoncuvi12@gmail.com>
2024-01-09 22:17:45 -0500

    Feat: Pagina de error añadida

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,30 @@
 
+import React from 'react'
+import CODITOW from '../services/CODITOW.png'
+import { useNavigate } from 'react-router-dom'
+import '../styles/Error.css'
 
 export const Error = () => {
+  const history = useNavigate();
+
+  const navigateTo = (path) => {
+      history(path);
+  }
+
   return (
-    <div>Error</div>
+     <div>
+            <div className='ErrorComponent container'>
+                <img className='imagenError' alt='ErrorImg' src={CODITOW} />
+                <div>
+                    <h1>
+                        Error 404
+                    </h1>
+                    <h2> La página que intentas acceder no existe o se ha movido </h2>
+                    <button className='btnInicio' onClick={() => navigateTo('/')} >Regresar a inicio</button>
+                <button className='btnInformar' onClick={() => navigateTo('/contact')} >Informar Error</button>
+                </div>
+            </div>
+
+        </div>
   )
 }
